c63cf23b37fe57e759ee38328a6f6243c975a37d
plion818 <patrick89818pp@gmail.com>
2025-06-13 17:40:46 +0800

    first

---
+++ AI_MSC_密度照片/blip2_finetune_and_predict.py
@@ -0,0 +1,83 @@
+import os
+import pandas as pd
+from PIL import Image
+from torch.utils.data import Dataset
+from transformers import Blip2Processor, Blip2ForConditionalGeneration, TrainingArguments, Trainer
+import torch
+
+# 1. 自訂Dataset
+class Blip2ImageTextDataset(Dataset):
+    def __init__(self, csv_file, processor):
+        self.data = pd.read_csv(csv_file)
+        self.processor = processor
+    def __len__(self):
+        return len(self.data)
+    def __getitem__(self, idx):
+        img_path = self.data.iloc[idx]['image_path']
+        text = self.data.iloc[idx]['text']
+        image = Image.open(img_path).convert('RGB')
+        inputs = self.processor(images=image, text=text, return_tensors="pt")
+        item = {k: v.squeeze(0) for k, v in inputs.items()}
+        item['labels'] = self.processor.tokenizer(text, return_tensors="pt").input_ids.squeeze(0)
+        return item
+
+# 2. 設定路徑
+base_dir = 'AI_MSC_密度照片'
+train_csv = os.path.join(base_dir, 'train_blip2.csv')
+test_csv = os.path.join(base_dir, 'test_blip2.csv')
+
+# 3. 載入BLIP-2
+model_name = "Salesforce/blip2-opt-2.7b"
+processor = Blip2Processor.from_pretrained(model_name)
+model = Blip2ForConditionalGeneration.from_pretrained(model_name)
+
+# 4. 準備資料集
+train_dataset = Blip2ImageTextDataset(train_csv, processor)
+test_dataset = Blip2ImageTextDataset(test_csv, processor)
+
+# 5. 訓練參數
+training_args = TrainingArguments(
+    output_dir="blip2_cell_growth_vlm",
+    per_device_train_batch_size=2,
+    per_device_eval_batch_size=2,
+    num_train_epochs=3,
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    logging_steps=10,
+    learning_rate=5e-5,
+    fp16=True,
+    report_to="none"
+)
+
+# 6. Trainer
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=test_dataset,
+)
+
+# 7. 微調
+trainer.train()
+
+# 8. 推論
+result_df = pd.read_csv(test_csv)
+pred_texts = []
+for idx, row in result_df.iterrows():
+    image = Image.open(row['image_path']).convert('RGB')
+    inputs = processor(images=image, return_tensors="pt").to(model.device)
+    generated_ids = model.generate(**inputs, max_new_tokens=20)
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    pred_texts.append(generated_text)
+result_df['predicted_text'] = pred_texts
+
+# 9. 根據描述判斷預測區間
+intervals = [d.replace('_','') for d in os.listdir(os.path.join(base_dir, 'train')) if os.path.isdir(os.path.join(base_dir, 'train', d))]
+def match_interval(text):
+    for interval in intervals:
+        if interval in text:
+            return interval
+    return '未知'
+result_df['predicted_interval'] = result_df['predicted_text'].apply(match_interval)
+result_df.to_csv(os.path.join(base_dir, 'test_blip2_predictions.csv'), index=False)
+print('微調與推論完成，結果已輸出到 test_blip2_predictions.csv')
